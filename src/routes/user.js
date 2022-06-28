e7df2acafe8818f393a04047969b889c7dbc438c
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-06-28 00:07:00 -0300

    fixing import pattern

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
-import express from 'express'
+import { Router } from 'express'
 import UserController from '../controllers/user.js'
 
-const router = express.Router()
+const router = new Router()
 
 router.get('/', UserController.getAllUsers)
 
