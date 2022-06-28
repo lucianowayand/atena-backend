0514c1e3471ba6aa7242e2a2e16e9af0c1aee256
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-06-27 23:56:16 -0300

    fixing json read error

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,8 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-export default class UserController{
-    static getAllUsers = async (req, res) => {
+export default class UserController {
+    static getAllUsers = async (_req, res) => {
         const users = await prisma.user.findMany()
         res.status(200).json(users)
     }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser';
 import UserController from './controllers/user.js';
 
 const app = express()
 const port = process.env.PORT || 5000;
+const jsonParser = bodyParser.json()
+
+app.use(jsonParser)
 
 app.get('/', (req, res) => {
     res.status(200).send({
