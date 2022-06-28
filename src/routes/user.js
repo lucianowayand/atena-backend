0d84eaf1ebe58230d402dc5fb9d0d1804898fc6f
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-06-28 07:28:13 -0300

    implementation of user CRUD

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,58 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+import bcrypt from 'bcrypt'
+
 import { Router } from 'express'
-import UserController from '../controllers/user.js'
 
 const router = new Router()
 
-router.get('/', UserController.getAllUsers)
+router.get('/', async (_req, res) => {
+    try{
+        const users = await prisma.user.findMany()
+        res.status(200).json({
+            users
+        })
+    } catch(error) {
+        res.status(404).json({
+            message: "Couldn't retrieve user info from the server"
+        })
+    }
+    
+})
+    
+router.post('/', async (req, res, next) => {
+    try{
+        let values = req.body.data
+        values.password = bcrypt.hashSync(values.password, 8)
+        const user = await prisma.user.create({
+            data: values
+        })
+        res.status(200).json({user})
+    } catch(error) {
+        res.status(400).json({
+            message: "Email already in use"
+        })
+    }
+    
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const id = req.params
+        const user = await prisma.user.delete({
+            where:{
+                id: Number(id)
+            }
+        })
+        res.status(400).json({
+            user
+        })
+    } catch(error){
+        res.status(400).json({
+            message: "Couldn't delete user"
+        })
+    }
+    
+})
 
 export default router
